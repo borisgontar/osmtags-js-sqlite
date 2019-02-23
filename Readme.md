177fdc273b7c4bdbbb3c9676463093c530215bc9
Boris Gontar <borisgontar@users.noreply.github.com>
2019-02-23 17:06:32 -0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,39 @@
+﻿# osmtags-js-sqlite
+
+This is a simple Node.js application to collect tags from an OpenStreetMap `osm.pbf` file into a SQLite3 database. It extracts all tags from the input file and creates a table in the target  database with the following data:
+* All distinct keys present in the tags.
+* For each key, all (well, almost) its distinct values. 
+* For each {key, value} pair, number of times this pair was found in nodes, ways and relations.
+
+The table is created as:
+```sql
+create table osmtags (
+  key text not null,   -- tag's key, e.g. 'amenity'
+  value text not null, -- tag's value, e.g. 'fuel'
+  n int default 0, -- number of times 'amenity=fuel' seen in nodes
+  w int default 0, --    same in ways
+  r int default 0, --    same in relations
+  primary key(key, value)
+);
+```
+Some keys, like `addr:street` can have too many values, so there is a limit for number of such values. When this limit is reached all {key, value} records for such key are coalesced into a single record with value "~". In this case the counts simply represent numbers of times the key was found in nodes, ways and relations, regardless of its value.
+
+Some keys can have multiple 'subkeys', like `addr:street`, `addr:housenumber`, etc. The application allows to coalesce such keys into a single one (`addr` in this example). 
+
+## Installation
+Install the dependencies:
+```bash
+npm install through2
+npm install osm-pbf-parser
+npm install better-sqlite3
+```
+Then just copy the source file `osmtags.js` into the directory where the dependencies were installed.
+
+## Usage
+Run the application as:
+```bash
+node ./osmtags.js options
+```
+where _options_ are:
+<dt>-f path-to-osm.pdf [another-path ...]</dt>
+<dd>input file(s) </dd>
---
+++ osmtags.js
@@ -0,0 +1,156 @@
+const fs = require('fs');
+const through = require('through2');
+const parseOSM = require('osm-pbf-parser');
+const Database = require('better-sqlite3');
+
+/** @type {{files:string, d:string, c:boolean, memory:number, l:number}} */
+const argv = require('yargs')
+    .options({
+        f: {
+            alias: 'files',
+            array: true,
+            describe: 'Path(s) to input file(s) .osm.pbf',
+            demand: true,
+            normalize: true
+        },
+        d: {
+            alias: 'database',
+            nargs: 1,
+            describe: 'Path to .sqlite database',
+            demand: true,
+            normalize: true
+        },
+        c: {
+            describe: 'Coalesce keys like name:xx into name',
+            boolean: true
+        },
+        l: {
+            alias: 'limit',
+            describe: 'Represent key with more than l values as single row with value "~"',
+            number: true,
+            default: 256
+        },
+        memory: {
+            describe: 'Cache size in Mb',
+            default: 1024,
+            number: true
+        },
+        h: {alias: 'help'}
+    })
+    .usage('Extracts all keys and their values from an OSM pbf file.')
+    .version(false)
+    .strict(true)
+    .argv;
+
+const db = new Database(argv.d, {verbose: null});
+db.pragma('journal_mode = WAL');
+db.pragma('foreign_keys = 1');
+db.pragma(`cache_size = -${argv.memory*1024}`);
+
+const create_stmt = db.prepare(
+    'create table if not exists osmtags(' +
+    'key text not null, value text not null,' +
+    'n int default 0, w int default 0, r int default 0,' +
+    'primary key(key, value))');
+const drop_stmt = db.prepare('drop table if exists osmtags');
+
+const table = {};
+
+function load() {
+    let stmt = db.prepare('select key, value, n, w, r from osmtags');
+    let count = 0;
+    for (let row of stmt.iterate()) {
+        let {key, value, n, w, r} = row;
+        if (!table[key])
+            table[key] = {};
+        table[key][value] = [n, w, r];
+        count += 1;
+    }
+    if (count > 0)
+        console.log('%d records loaded', count);
+}
+
+function store() {
+    const ins_stmt = db.prepare('insert into osmtags values(?,?,?,?,?)');
+    drop_stmt.run();
+    create_stmt.run();
+    db.prepare('begin').run();
+    for (let key of Object.keys(table).sort()) {
+        let row = table[key];
+        for (let value of Object.keys(row).sort()) {
+            let c = row[value];
+            ins_stmt.run(key, value, c[0], c[1], c[2]);
+        }
+    }
+    db.prepare('commit').run();
+}
+
+/**
+ * @param {string} file
+ * @param {Function} callback
+ * @returns {Promise}
+ */
+function scan(file, callback) {
+    return new Promise(resolve => {
+        fs.createReadStream(file)
+            .pipe(parseOSM())
+            .pipe(through.obj(
+                (items, enc, next) => {
+                    for (let item of items)
+                        callback(item);
+                    next();
+                }))
+            .on('finish', resolve);
+    });
+}
+
+create_stmt.run();
+load();
+console.time('elapsed');
+pass().then(() => {
+    store();
+    db.close();
+    console.timeEnd('elapsed');
+});
+
+async function pass() {
+    for (let file of argv.files) {
+        console.log('reading ' + file);
+        let count = 0;
+        await scan(file, item => {
+            let {type, tags} = item;
+            let j = type == 'node' ? 0 : type == 'way' ? 1 : 2;
+            for (let key in tags) if (tags.hasOwnProperty(key)) {
+                let value = tags[key];
+                if (argv.c) {
+                    let i = key.indexOf(':');
+                    if (i > 0)
+                        key = key.substring(0, i);
+                }
+                if (!table[key])
+                    table[key] = {};
+                const row = table[key];
+                if (row['~'])
+                    row['~'][j] += 1;
+                else if (row[value])
+                    row[value][j] += 1;
+                else if (Object.keys(row).length <= argv.l) {
+                    row[value] = [0, 0, 0];
+                    row[value][j] += 1;
+                } else {
+                    let acc = [0, 0, 0];
+                    for (let v in row) {
+                        acc[0] += row[v][0];
+                        acc[1] += row[v][1];
+                        acc[2] += row[v][2];
+                    }
+                    acc[j] += 1;
+                    table[key] = {'~': acc};
+                }
+            }
+            if ((++count % 1000000) === 0)
+                process.stdout.write('objects: ' + count + '\r');
+        });
+        console.log('objects:', count);
+    }
+}
